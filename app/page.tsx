198af118c8d6c04c98844adba8d83a9312af9064
rizakhanoff <nf@rizakhanoff.com>
2024-10-23 20:40:46 +0300

    modified:   app/page.tsx 	modified:   package.json

--- app/page.tsx
+++ app/page.tsx
@@ -9,19 +9,34 @@ import { GithubIcon } from "@/components/icons";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-        <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </div>
-      </div>
-  
+    <>
+    
+    
+    
+    
+    <section className="block md:hidden pt-8 md:pt-10">
+      <span className={title()}>Добро пожаловать в&nbsp;</span>
+      <br />
+      <span className={title({ color: "blue" })} style={{ fontFamily: "Audiowide", }}>VillaMogoh&nbsp;</span>
     </section>
+    
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-xl text-center justify-center">
+          <span className={title()}>Make&nbsp;</span>
+          <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
+          <br />
+          <span className={title()}>
+            websites regardless of your design experience.
+          </span>
+          <div className={subtitle({ class: "mt-4" })}>
+            Beautiful, fast and modern React UI library.
+          </div>
+        </div>
+
+      </section>
+      
+      
+      
+      </>
   );
 }
