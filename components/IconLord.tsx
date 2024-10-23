36b8d15577d6c8159248032cc51a97ed3defd583
rizakhanoff <nf@rizakhanoff.com>
2024-10-23 22:01:05 +0300

    modified:   app/layout.tsx 	modified:   app/page.tsx 	new file:   components/IconLord.tsx 	modified:   package.json 	new file:   public/confetti.json

---
+++ components/IconLord.tsx
@@ -0,0 +1,43 @@
+'use client'
+ 
+import { Player } from "@lordicon/react";
+import { useEffect, useRef, useState } from 'react';
+
+
+async function loadIcon(name: string) {
+    const request = await fetch(`/${name}.json`);
+    const data = await request.json();
+    return data;
+}
+
+export default function Icon() {
+    const initialized = useRef(false);
+    const playerRef = useRef<Player>(null);
+    const [data, setData] = useState<any>(null)
+
+    useEffect(() => {
+        if (initialized.current) {
+            return;   
+        }
+
+        initialized.current = true;
+
+        loadIcon('confetti').then((iconData) => {
+            setData(iconData);
+        });
+    }, []);
+    
+    const playerReady = () => {
+        playerRef.current?.playFromBeginning();
+    }
+
+    const iconClick = () => {
+        playerRef.current?.playFromBeginning();
+    }
+
+    return (
+        <div onClick={iconClick}>
+            <Player ref={playerRef} icon={data} size={128}   /> 
+        </div>
+    )
+}
