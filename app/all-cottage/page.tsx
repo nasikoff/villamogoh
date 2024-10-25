beda0c3e941cd4d779883e72c2ebc8c3ba71dee8
rizakhanoff <nf@rizakhanoff.com>
2024-10-25 19:46:25 +0300

    modified:   app/all-cottage/page.tsx 	modified:   config/site.tsx

--- app/all-cottage/page.tsx
+++ app/all-cottage/page.tsx
@@ -16,7 +16,34 @@ export default function AllCottage() {
 
     <div className="gap-3 grid grid-cols-2 sm:grid-cols-2">
     
-      
+      {cottage.map((item) => (
+
+ 
+
+                <Card key={""} shadow="sm"   isPressable className="py-4 border-transparent backdrop-blur-lg backdrop-saturate-[1.8] bg-white dark:bg-default-400/10">
+                <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                  <p className="text-large uppercase font-bold">{item.title}</p>
+                </CardHeader>
+                <CardBody className="overflow-visible py-2">
+                <Image
+                    shadow="sm"
+                    radius="lg"
+                    width="100%"
+                    height="300px"
+                    alt={item.title}
+                    className="w-full object-cover h-[140px]"
+                    src={item.img}
+                   />
+                  <p className="m-5 ml-0">{item.description}</p>
+                </CardBody>
+
+                  <CardFooter>
+                    <p>{item.price}</p>
+                  </CardFooter>
+                </Card>
+
+
+      ))}
     </div>
  
     </section>
