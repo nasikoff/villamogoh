6f13e25e979422e66216c76a86b6a96dce5dee74
rizakhanoff <nf@rizakhanoff.com>
2024-11-03 00:02:50 +0300

    modified:   app/all-cottage/cottage-one/page.tsx 	modified:   app/page.tsx 	new file:   components/BookingButton.tsx 	deleted:    components/ButtonPlus copy.tsx 	modified:   components/ButtonPlus.tsx 	new file:   components/MapsButton.tsx 	modified:   components/icons.tsx 	modified:   config/site.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { PriceOne, PriceRB, PriceTwo, siteConfig } from "@/config/site";
 import { title } from "@/components/primitives";
-import { Button, Card, Image, CardBody, CardFooter, CardHeader, CircularProgress, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger } from "@nextui-org/react";
+import { Button, Card, Image, CardBody, CardFooter, CardHeader, CircularProgress, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger, Link, Chip } from "@nextui-org/react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode, Pagination } from 'swiper/modules';
 import { Navigation } from 'swiper/modules';
@@ -13,6 +13,9 @@ import "swiper/css";
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import YandexMaps from "@/components/YandexMaps";
+import BookingButton from "@/components/BookingButton";
+import MapsButton from "@/components/MapsButton";
+
 export default function Home() {
 
   return (
@@ -31,24 +34,9 @@ export default function Home() {
          <br />
          <p className="w-full md:w-11/12 my-2 text-lg lg:text-lg pt-6 pb-4 font-normal text-default-500 block max-w-full">VILLAMOGOH - идеальное место для отдыха в окружении природы. Наши уютные коттеджи с видом на величественные горы подарят вам незабываемые впечатления. <br/> Забронируйте свой отдых уже сегодня!</p>
          <br />
-         <Dropdown backdrop="blur">
-            <DropdownTrigger>
-               <Button color="primary">
-                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><path fill="currentColor" d="M17.616 16.385V18.5q0 .154.115.27q.115.114.269.114t.27-.115q.114-.115.114-.269v-2.116H20.5q.154 0 .27-.115q.114-.115.114-.269t-.115-.27q-.115-.114-.269-.114h-2.116V13.5q0-.154-.115-.27q-.115-.114-.269-.114t-.27.115q-.114.115-.114.269v2.116H15.5q-.154 0-.27.115q-.114.115-.114.269t.115.27q.115.114.269.114zM18 20q-1.671 0-2.835-1.164Q14 17.67 14 16t1.165-2.835T18 12t2.836 1.165T22 16t-1.164 2.836T18 20M5 16.384V8.308q0-.384.172-.727q.171-.344.474-.566l5.385-4.077q.423-.323.966-.323t.972.323l5.385 4.078q.303.222.474.566q.172.345.172.73v.425q0 .223-.17.385t-.413.132q-1.425-.108-2.812.414q-1.388.523-2.401 1.536q-1.177 1.177-1.665 2.727t-.198 3.144q.067.373-.154.649t-.57.276H6.616q-.667 0-1.141-.475T5 16.386"></path></svg>
-                  Забронировать
-               </Button>
-            </DropdownTrigger>
-            <DropdownMenu  aria-label="Static Actions">
-               <DropdownSection title="Выберите объекты" showDivider>
-                  <DropdownItem key="new" href="#">Сottege One</DropdownItem>
-                  <DropdownItem key="copy">Сottege Two</DropdownItem>
-                  <DropdownItem key="copy">Russian Bathhouse</DropdownItem>
-                  <DropdownItem key="delete" className="text-danger" color="danger">
-                     Закрыть окно
-                  </DropdownItem>
-               </DropdownSection>
-            </DropdownMenu>
-         </Dropdown>
+
+         <BookingButton text={"Забронировать"}  />
+
       </div>
       <div className="flex-auto w-full  sm:w-80">
          <Swiper 
@@ -126,30 +114,12 @@ export default function Home() {
                   src="/img/11.webp"
                   />
                  <CardFooter className="justify-between py-1 absolute before:rounded-xl rounded-large bottom-1 ml-1 z-10">
-                  <Button className="text-tiny text-white bg-white/20" variant="flat" color="default" radius="lg" size="sm">
-                  Подробнее
-                  </Button>
+                   <Chip size="sm">Подробнее</Chip>
                </CardFooter>
             </Card>
          </div>
          <div >
-            <Card className="col-span-12 sm:col-span-4 h-[200px]">
-               <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-                  <h4 className=" font-medium text-black text-large uppercase">Мы здесь</h4>
-               </CardHeader>
-               <Image
-                  isZoomed
-                  removeWrapper
-                  alt="Card background"
-                  className="z-0 w-full h-full object-cover hue-rotate-60"
-                  src="/img/maps.png"
-                  />
-               <CardFooter className="justify-between py-1 absolute before:rounded-xl rounded-large bottom-1 ml-1 z-10">
-                  <Button className="text-tiny text-black bg-black/20" variant="flat" color="default" radius="lg" size="sm">
-                  Построить маршрут
-                  </Button>
-               </CardFooter>
-            </Card>
+         <MapsButton text={""}/>
          </div>
       </div>
       </div>
---
+++ components/MapsButton.tsx
@@ -0,0 +1,78 @@
+import { FC } from 'react';
+import { Button } from "@nextui-org/button";
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Card, CardFooter, Image, CardHeader, Chip } from "@nextui-org/react";
+import { BroneIcon2 } from "./icons";
+
+interface MapsButtonProps {
+  text: string;
+}
+
+const MapsButton: FC<MapsButtonProps> = ({ text }) => {
+  const items = [
+    {
+        key: "new",
+        label: "Яндекс Карты",
+        svg: <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 16 16"><path fill="currentColor" fill-rule="evenodd" d="M13.5 8a5.5 5.5 0 1 1-11 0a5.5 5.5 0 0 1 11 0M15 8A7 7 0 1 1 1 8a7 7 0 0 1 14 0M6.136 5.103a.75.75 0 0 0-1.272.795l2.044 3.27c.223.357.342.77.342 1.192v1.14a.75.75 0 0 0 1.5 0v-1.14a3.75 3.75 0 0 0-.57-1.987zm5 .795a.75.75 0 1 0-1.272-.795L8.77 6.853a.75.75 0 0 0 1.272.795z" clip-rule="evenodd"/></svg>,
+    },
+    {
+        key: "copy",
+        label: "2 Гис",
+        svg: <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 16 16"><path fill="currentColor" d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6a3 3 0 0 1 0 6"/></svg>,
+    },
+    {
+        key: "copy",
+        label: "Google Maps",
+        svg: <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"><path fill="currentColor" d="M6 12a6 6 0 0 0 11.659 2H12v-4h9.805v4H21.8c-.927 4.564-4.962 8-9.8 8c-5.523 0-10-4.477-10-10S6.477 2 12 2a9.99 9.99 0 0 1 8.282 4.393l-3.278 2.295A6 6 0 0 0 6 12"/></svg>,
+    },
+
+    {
+        key: "delete",
+        label: "Закрыть окно",
+        svg: <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275t.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275z"/></svg>,
+    }
+  ];
+
+  return (
+    <Dropdown backdrop="blur">
+      <DropdownTrigger>
+        
+
+      <Card className="col-span-12 sm:col-span-4 h-[200px] cursor-pointer">
+               <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+                  <h4 className=" font-medium text-black text-large uppercase">Мы здесь</h4>
+               </CardHeader>
+               <Image
+                  isZoomed
+                  removeWrapper
+                  alt="Card background"
+                  className="z-0 w-full h-full object-cover hue-rotate-15"
+                  src="/img/maps.png"
+                  />
+               <CardFooter className="justify-between py-1 absolute before:rounded-xl rounded-large bottom-1 ml-1 z-10">
+               <Chip size="sm">Построить маршрут</Chip>
+               </CardFooter>
+            </Card>
+
+
+
+
+
+
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Dynamic Actions" items={items}>
+        {(item) => (
+          <DropdownItem
+            key={item.key}
+            color={item.key === "delete" ? "danger" : "default"}
+            className={item.key === "delete" ? "text-danger" : ""}
+          >
+          <div className='flex gap-2'>{item.svg}{item.label}</div>
+            
+          </DropdownItem>
+        )}
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
+
+export default MapsButton;
