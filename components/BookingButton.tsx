60957bd8a7df5fc912ac1a830a3fc946979721e2
rizakhanoff <nf@rizakhanoff.com>
2024-11-05 00:06:37 +0300

    modified:   components/BookingButton.tsx

--- components/BookingButton.tsx
+++ components/BookingButton.tsx
@@ -205,7 +205,7 @@ const BookingButton: FC<BookingButtonProps> = ({ text }) => {
 
                   </Card>
                 </ModalBody>
-               
+                
               </>
           </ModalContent>
         </Modal>
@@ -290,7 +290,9 @@ const BookingButton: FC<BookingButtonProps> = ({ text }) => {
                   </ol>
             </ScrollShadow>
               </ModalBody>
-               
+              <ModalFooter className='pb-8 pt-2 font-normal'>
+                   Следуя этим правилам, вы способствуете созданию комфортной и безопасной атмосферы для всех гостей. Приятного отдыха!
+                </ModalFooter>
         
             </>
       
@@ -420,9 +422,7 @@ const BookingButton: FC<BookingButtonProps> = ({ text }) => {
 
                   </Card>
                 </ModalBody>
-                <div className='pb-8 pt-0 py-4'>
-                   Прежде чем совершить бронирование, рекомендуем внимательно изучить <div onClick={openTermsModal} className="text-warning underline">условия проживания</div> 
-                </div>
+               
                
               </>
           </ModalContent>
@@ -503,7 +503,7 @@ const BookingButton: FC<BookingButtonProps> = ({ text }) => {
 
                   </Card>
                 </ModalBody>
-               
+                
                
               </>
 
